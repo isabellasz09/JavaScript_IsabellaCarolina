8b8c7e3eab895efc3f9db6096b3f060196988b82
isabellasz09 <isabellacarolinadesouza17@gmail.com>
2025-07-03 22:12:29 -0300

    commit

---
+++ formularioSaude.js
@@ -0,0 +1,42 @@
+//executar mascaras
+
+function mascara(o,f){
+    //define o objeto e chama a função
+    objeto=o
+    funcao=f
+    setTimeout("executaMascara()",1)
+
+}
+
+function executaMascara(){
+    objeto.value=funcao(objeto.value)
+}
+
+//mascaras
+//telefone
+function telefone(variavel){
+    variavel=variavel.replace(/\D/g,"")
+    variavel=variavel.replace(/^(\d\d)(\d)/g,"($1) $2") 
+    //adiciona parenteses em volta dos dois primeiros digitos
+    variavel=variavel.replace(/(\d{4})(\d)/,"$1-$2") 
+    //adiciona hifem entre o quarto e o quinto digito 
+    return variavel
+}
+
+function RGeCPF(variavel){
+    variavel=variavel.replace(/\D/g,"")//remove caracteres não numericos
+    variavel=variavel.replace(/(\d{3})(\d)/,"$1.$2") 
+    //adiciona ponto entre o terceiro e o quarto digito
+    variavel=variavel.replace(/(\d{3})(\d)/,"$1.$2") 
+    //adiciona ponto entre o sexto e o setimo digito
+    variavel=variavel.replace(/(\d{3})(\d{1,2})$/,"$1-$2") 
+    //adiciona hifem entre o quarto e o quinto digito 
+    return variavel
+}
+
+function cep(variavel){
+    variavel=variavel.replace(/\D/g,"")//remove caracteres não numericos
+    variavel=variavel.replace(/(\d{2})(\d)/,"$1.$2") 
+    variavel=variavel.replace(/(\d{3})(\d)/,"$1-$2") 
+    return variavel
+}
